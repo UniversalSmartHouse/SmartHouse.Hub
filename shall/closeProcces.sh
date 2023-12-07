08aee01019f5483086e805376fb414a87015d228
Katyara11 <maksymm.kacharaba@gmail.com>
2023-12-07 22:43:27 +0200

    add closeProcces

---
+++ shall/closeProcces.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DB_FILE_PATH="$DB_FILE_PATH"
+
+if [ -z "$DB_FILE_PATH"]; then
+    echo "DB_FILE_PATH is null"
+    exit 1
+fi
+
+PIDS=$(lsof -t "$DB_FILE_PATH")
+
+if [ -z "$PIDS" ]; then
+    echo "not procces $DB_FILE_PATH"
+    exit 0
+fi
+
+for PID in $PIDS; do
+    kill $PID
+done
+
+echo "closed procces $DB_FILE_PATH"
