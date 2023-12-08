190f6672ecd722ab54d43bcbc603c6ac70c5a253
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2023-12-08 21:45:42 +0200

    Update InstanceController.cs

--- SmartHouseHub.API/Controllers/InstanceController.cs
+++ SmartHouseHub.API/Controllers/InstanceController.cs
@@ -1,13 +1,105 @@
+using Affiliates.API.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using SmartHouseHub.API.Dto;
+using SmartHouseHub.API.Interfaces;
 
 namespace SmartHouseHub.API.Controllers
 {
 	[ApiController]
 	[Route("[controller]")]
-	public class InstanceController : ControllerBase
+	public class InstanceController : Controller
 	{
-		public InstanceController()
+		private readonly IInstanceService _instanceService;
+		private readonly ILogService _logService;
+
+		public InstanceController(IInstanceService instanceService, ILogService logService)
 		{
+			_instanceService = instanceService;
+			_logService = logService;
+		}
+
+		[HttpGet("")]
+		public async Task<List<InstanceDto>> GetAll()
+		{
+			try
+			{
+				return await _instanceService.GetAll();
+			}
+			catch (Exception ex)
+			{
+			 	await _logService.Insert(new()
+				{
+					Id = Guid.NewGuid(),
+					Message = ex.Message,
+					Type = LogType.Error,
+					Timestamp = DateTime.UtcNow,
+				});
+
+				throw;
+			}
+		}
+
+		[HttpGet("{id}")]
+		public async Task<InstanceDto> GetById(Guid id)
+		{
+			try
+			{
+				return await _instanceService.GetById(id);
+			}
+			catch (Exception ex)
+			{
+				await _logService.Insert(new()
+				{
+					Id = Guid.NewGuid(),
+					Message = ex.Message,
+					Type = LogType.Error,
+					Timestamp = DateTime.UtcNow,
+				});
+
+				throw;
+			}
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<DeleteDto> DeleteById(Guid id)
+		{
+			try
+			{
+				return await _instanceService.DeleteById(id);
+			}
+			catch (Exception ex)
+			{
+				await _logService.Insert(new()
+				{
+					Id = Guid.NewGuid(),
+					Message = ex.Message,
+					Type = LogType.Error,
+					Timestamp = DateTime.UtcNow,
+				});
+
+				throw;
+			}
+		}
+
+		[HttpPost("")]
+		public async Task<InstanceDto> Insert([FromBody] InstanceDto obj)
+		{
+			try
+			{
+				return await _instanceService.Insert(obj);
+			}
+			catch (Exception ex)
+			{
+				await _logService.Insert(new()
+				{
+					Id = Guid.NewGuid(),
+					Message = ex.Message,
+					Type = LogType.Error,
+					Timestamp = DateTime.UtcNow,
+				});
+
+				throw;
+			}
 		}
 	}
 }
