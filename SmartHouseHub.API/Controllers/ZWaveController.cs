a6761c88712f70cba0f5a4adea66c4b1f59b0e2a
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2023-12-16 11:45:28 +0200

    Add ZWaveBrokerService & ZWaveController

---
+++ SmartHouseHub.API/Controllers/ZWaveController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartHouseHub.API.Interfaces;
+using ZWaveLib;
+
+namespace SmartHouseHub.API.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class ZWaveController : Controller
+	{
+		private readonly IZWaveBrokerService _zWaveBrokerService;
+		public ZWaveController(IZWaveBrokerService zWaveBrokerService)
+        {
+            _zWaveBrokerService = zWaveBrokerService;
+        }
+
+		[HttpGet("id")]
+		public async Task<ZWaveNode> GetNodeById(byte id)
+		{
+			try
+			{
+				return await _zWaveBrokerService.GetNodeById(id);
+			}
+			catch (Exception ex)
+			{
+				throw;
+			}
+		}
+
+		[HttpGet("")]
+		public async Task<List<ZWaveNode>> GetAllNode()
+		{
+			try
+			{
+				return await _zWaveBrokerService.GetAllNode();
+			}
+			catch (Exception ex)
+			{
+				throw;
+			}
+		}
+
+		[HttpGet("/Swith{state}")]
+		public async Task SendBinerySwith(bool state)
+		{
+			try
+			{
+				await _zWaveBrokerService.SendBinerySwith(state);
+			}
+			catch (Exception ex)
+			{
+				throw;
+			}
+		}
+	}
+}
---
+++ SmartHouseHub.API/Servises/ZWaveBrokerService.cs
@@ -0,0 +1,40 @@
+﻿using SmartHouseHub.API.Interfaces;
+using ZWaveLib;
+
+namespace SmartHouseHub.API.Servises
+{
+	public class ZWaveBrokerService : IZWaveBrokerService
+	{
+		private readonly IZWaveBroker _broker;
+
+		public ZWaveBrokerService(IZWaveBroker broker)
+		{
+			_broker = broker;
+		}
+
+		public async Task<List<ZWaveNode>> GetAllNode()
+		{
+			return _broker.GetAllNode();
+		}
+
+		public async Task<ZWaveNode> GetNodeById(byte nodeId)
+		{
+			return _broker.GetNodeById(nodeId);
+		}
+
+		public async Task SendBinerySwith(bool state)
+		{
+			var allNodes = _broker.GetAllNode();
+
+			foreach (var node in allNodes)
+			{
+				_broker.SendBinarySwitchCommand(node.Id, state);
+			}
+		}
+
+		public async Task Stop()
+		{
+			_broker.Dispose();
+		}
+	}
+}
