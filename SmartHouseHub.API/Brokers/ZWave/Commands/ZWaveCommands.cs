6b4f064687e64826decf941311dfb0a07024794b
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2023-12-16 15:38:04 +0200

    fix folder in brokers folder

---
+++ SmartHouseHub.API/Brokers/ZWave/Commands/ZWaveCommands.cs
@@ -0,0 +1,16 @@
+﻿using SmartHouseHub.API.Interfaces;
+using ZWaveLib;
+
+namespace SmartHouseHub.API.Brokers.ZWave.Commands
+{
+    public class ZWaveCommands : IZWaveCommands
+    {
+        public void SendBinarySwitchCommand(ZWaveNode node, bool state)
+        {
+            if (node.SupportCommandClass(CommandClass.SwitchBinary))
+            {
+                node.SendDataRequest(ZWaveUtilsCommands.SwitchBinary(state));
+            }
+        }
+    }
+}
