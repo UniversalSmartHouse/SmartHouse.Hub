a5b77a322f2a1847643560b6d0ce95afdc884ead
Katyara11 <maksymm.kacharaba@gmail.com>
2023-12-17 20:19:07 +0200

    delete syncJs

---
+++ SmartHouseHub.API/DTOs/BaseCouchbaseDocDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartHouseHub.API.DTOs
+{
+    public class BaseCouchbaseDocDto
+    {
+        public required string Creator { get; set; }
+        public required string Title { get; set; }
+    }
+}
